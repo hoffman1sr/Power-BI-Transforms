0214e5a9970946d183b8d8c28a8beff2326cc4c7
hoffman1sr <hoffman1sr@gmail.com>
2022-11-15 15:49:03 -0500

    Added comments and took out unnecessary print

--- Transform Aggregated Measures Table.py
+++ Transform Aggregated Measures Table.py
@@ -1,15 +1,18 @@
 import pandas as pd
 df_AggMeasures = pd.read_csv(r'C:\Users\SHoffman\Downloads\Aggregated Measures Table.csv')
 
+# Get scenarios and create the new columns by getting the scenarios from measure column
 scenarios = df_AggMeasures['Scenario'].drop_duplicates().values
 newcolumns = ['Year']
 newcolumns.extend( df_AggMeasures['measure'].drop_duplicates().values.tolist() )
 newcolumns.append('Scenario')
 
+# This will tranpose over scenario at a time. 
 def transposer(scenario, df):  
-    temp = df.loc[ df['Scenario']== scenario ].T.reset_index()
-    temp['Scenario'] = scenario
+    temp = df.loc[ df['Scenario']== scenario ].T.reset_index() # Rotate the data
+    temp['Scenario'] = scenario # Create a new column with the current scenario name
 
+# Get the the new transposed column names, and the desired column names. Add to dictionary and rename columns.
     columns_dict = {}
     i=0
     for entry in temp.columns:
@@ -20,9 +23,8 @@ def transposer(scenario, df):
     temp = temp.iloc[2:][:]
     return temp
 
+# Transpose the chunks for each scenario and concat it all together
 final_df = pd.DataFrame(columns=newcolumns)
 for scenario in scenarios:
     temp = transposer(scenario, df_AggMeasures)
     final_df = pd.concat([final_df, temp])
-    
-print(final_df)
